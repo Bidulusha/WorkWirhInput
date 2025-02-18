15fa8ec71cdd96898409d0c6618c64afa0754067
Bidulusha <74060730+Bidulusha@users.noreply.github.com>
2025-02-18 15:37:22 +0300

    intToString

--- cFiles/objects/Functions/additional.h
+++ cFiles/objects/Functions/additional.h
@@ -1,9 +1,32 @@
 #pragma once
 
+#include <stdio.h>
+#include <locale.h>
+#include <stdio.h>
+#include <math.h>
+
+#ifdef _WIN32
+#include <conio.h>
+#include <windows.h>
+#include <io.h>
+
+#define write _write
+#else
+#include <unistd.h>
+
+#endif
+
 #define clearstd fflush(stdout)
 
+char * gos(char str){ //getOneSymbol
+    char * new_str = (char*)malloc(2 * sizeof(char));
+    new_str[0] = str;
+    new_str[1] = '\0';
+    return new_str;
+}
+
 unsigned int fileSize(char * filename){
-    #include <stdio.h>
+    
 
     char buffer[256];
     int count = 0;
@@ -32,4 +55,22 @@ void print(const char *text, const char * end) {
 
 void println(const char *text) {
     print(text, "\n");  
+}
+
+char * intToString(int n){
+    int temp = n, size = 1;
+    while(temp){temp /= 10; size += 1;}
+
+    char * new = (char*)malloc((size) * sizeof(char));
+    new[0] = '\0';
+
+    size -= 2;
+
+    while(n){
+        strcat(new, gos(48 + n / ((int)pow(10, size))));
+        n %= ((int)pow(10,size));
+        size -= 1;
+    }
+
+    return new;
 }
